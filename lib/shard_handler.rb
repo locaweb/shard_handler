fb621255776add5998eb157e96d623267b3f5184
Lenon Marcel <lenon.marcel@gmail.com>
2015-06-29 18:21:54 -0300

    Create the setup method
    This method is expected to be used in an initializer and receives a
    hash of shards configs.

--- lib/shard_handler.rb
+++ lib/shard_handler.rb
@@ -3,5 +3,35 @@ require 'shard_handler/model'
 require 'shard_handler/cache'
 
 module ShardHandler
-  # Your code goes here...
+  class InvalidShardName < StandardError; end
+
+  class << self
+    def setup(shards_config)
+      Cache.connection_handlers = {}
+
+      shards_names = shards_config.keys.map(&:to_sym)
+      shards_names.each do |name|
+        cache_connection_handler(name, shards_config)
+      end
+    end
+
+    protected
+
+    def cache_connection_handler(shard_name, configs)
+      spec = resolver_class.new(configs).spec(shard_name)
+
+      handler = connection_handler_class.new
+      handler.establish_connection(Model, spec)
+
+      Cache.connection_handlers[shard_name] = handler
+    end
+
+    def resolver_class
+      ActiveRecord::ConnectionAdapters::ConnectionSpecification::Resolver
+    end
+
+    def connection_handler_class
+      ActiveRecord::ConnectionAdapters::ConnectionHandler
+    end
+  end
 end
