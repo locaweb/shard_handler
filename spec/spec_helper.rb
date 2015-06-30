56b9d06dee1e8eaee124cffe5236b6a22f00f8a7
Lenon Marcel <lenon.marcel@gmail.com>
2015-06-30 17:47:59 -0300

    Clean up current shard before spec execution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,4 +96,8 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+
+  config.before do
+    ShardHandler.current_shard = nil
+  end
 end
