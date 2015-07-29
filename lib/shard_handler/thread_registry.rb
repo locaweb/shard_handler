0078933854131e07e714434efb5645d63fc39e43
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-29 14:47:49 -0300

    Add documentation for ThreadRegistry

--- lib/shard_handler/thread_registry.rb
+++ lib/shard_handler/thread_registry.rb
@@ -1,6 +1,12 @@
 require 'active_support/per_thread_registry'
 
 module ShardHandler
+  # This class is used as a registry for the shard name that is being used in
+  # the current thread. Because ActiveRecord's connections are global in the
+  # thread scope, we need to make sure that the shard name is change only for
+  # the current thread and not on process or other threads.
+  #
+  # @see ActiveSupport::PerThreadRegistry
   class ThreadRegistry
     extend ActiveSupport::PerThreadRegistry
     attr_accessor :current_shard
