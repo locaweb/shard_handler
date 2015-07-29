585f966f4557043d35b5580afd10fa8eafed6e94
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-29 17:14:14 -0300

    Add documentation for Model

--- lib/shard_handler/model.rb
+++ lib/shard_handler/model.rb
@@ -5,29 +5,52 @@ module ShardHandler
     self.abstract_class = true
 
     class << self
+      # @api private
+      # @return [Handler]
       def handler
         @@handler
       end
 
+      # This method creates an instance of {Handler} for this class. This method
+      # must be called before performing any query on shards.
+      #
+      # @param config [Hash] a hash with database connection settings
       def setup(config)
         @@handler = Handler.new(self, config)
         @@handler.setup
       end
 
+      # Returns the current shard name for the current Thread.
+      #
+      # @return [Symbol]
       def current_shard
         ThreadRegistry.current_shard
       end
 
+      # Sets the current shard name for the current Thread.
+      #
+      # @param name [Symbol, String] shard name configured using {.setup}
       def current_shard=(name)
         ThreadRegistry.current_shard = name.nil? ? nil : name.to_sym
       end
 
+      # Overrides ActiveRecord::Core#connection_handler method to return the
+      # appropriate ConnectionHandler for the current shard. This is the
+      # integration point between ActiveRecord and this gem.
+      #
+      # @api private
+      # @return (see Handler#connection_handler_for)
       def connection_handler
         fail(SetupError, 'the model was not setup') unless @@handler
         @@handler.connection_handler_for(current_shard) || super
       end
 
-      def using(shard, &_block)
+      # This method will switch to the passed shard making all queries be
+      # executed using the shard connection.
+      #
+      # @param shard [Symbol, String] shard name configured using .setup
+      # @yield The block that must be executed using the shard connection
+      def using(shard)
         old_shard = current_shard
         self.current_shard = shard
         yield
