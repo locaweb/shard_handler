96680a0ab1fff1ce27c95c87c9a341916d1e255b
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-29 14:10:15 -0300

    Add documentation for error classes

--- lib/shard_handler/errors.rb
+++ lib/shard_handler/errors.rb
@@ -1,4 +1,9 @@
 module ShardHandler
+  # This error is raised when one tries to perform a query on {Model} without
+  # setting it up first.
   class SetupError < StandardError; end
+
+  # This error is raised when using a shard name that does not exist in the
+  # configuration.
   class UnknownShardError < StandardError; end
 end
