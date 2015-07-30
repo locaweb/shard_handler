d697e2acbf4e9574b0591d94a99cc94aa1d9e1d5
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-30 13:35:52 -0300

    Fix typo

--- lib/shard_handler/thread_registry.rb
+++ lib/shard_handler/thread_registry.rb
@@ -3,7 +3,7 @@ require 'active_support/per_thread_registry'
 module ShardHandler
   # This class is used as a registry for the shard name that is being used in
   # the current thread. Because ActiveRecord's connections are global in the
-  # thread scope, we need to make sure that the shard name is change only for
+  # thread scope, we need to make sure that the shard name is changed only for
   # the current thread and not on process or other threads.
   #
   # @see ActiveSupport::PerThreadRegistry
